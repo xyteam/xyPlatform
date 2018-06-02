a9006c84bb59bcf96e455210cd67adbdc0c78006
xywang68 <xywang68@users.noreply.github.com>
2018-06-01 18:23:59 -0700

    Update runSeleniumStandalone.bat

--- windows10/runSeleniumStandalone.bat
+++ windows10/runSeleniumStandalone.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 cd %HOMEPATH%
-start "selenium-standalone" /MIN node_modules\.bin\selenium-standalone start --config=Documents\selenium-standalone-config.js
+start "selenium-standalone" /MIN node_modules\.bin\selenium-standalone start --config=selenium-standalone-config.js
 EXIT /b 0
