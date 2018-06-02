4d9b22ab5f7f19fab029b73dc08bffc9e79aee63
xywang68 <xywang68@users.noreply.github.com>
2018-06-01 19:24:45 -0700

    Update setUnblockApps.bat

--- windows10/setUnblockApps.bat
+++ windows10/setUnblockApps.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 netsh advfirewall firewall add rule name="Java(TM) Platform SE binary" dir=in action=allow
-netsh advfirewall firewall add rule name="Command line server for the IE driver" dir=in action=allow
+netsh advfirewall firewall add rule name="Command line server for the IE driver" dir=in program="C:\users\ieuser\node_modules\selenium-standalone\.selenium\iedriver\3.4.0-x64-iedriverserver.exe" action=allow
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 0 /f
-reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
-EXIT
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d "Off" /f
+reg add "HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" /v EnabledV9 /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\AppHost" /v EnableWebContentEvaluation /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
