0766ea16b25b937a4a1e4844bb44daa5266cc35b
xywang68 <xywang68@gmail.com>
2018-09-18 18:23:27 -0700

    turn off screensavers; create opencv_java link needed for SikulixAPI

--- lubuntu/apt_install.sh
+++ lubuntu/apt_install.sh
@@ -1,6 +1,9 @@
 echo "installing apt packages"
 export DEBIAN_FRONTEND=noninteractive
-apt update -q && apt autoremove -y
+apt update -q
+apt purge -q -y gnome-screensaver xscreensaver
+apt autoremove -y
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" lxde-core lubuntu-core ntpdate autofs tree xvfb xdotool python python-pip software-properties-common wmctrl xclip ffmpeg sshfs rdesktop imagemagick nodejs npm
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" build-essential module-assistant virtualbox-guest-dkms g++ gyp git-crypt dkms tesseract-ocr linux-headers-$(uname -r) unixodbc unixodbc-dev tdsodbc sqlite3 freetds-bin libnss3 libnss3-tools
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libssl-dev libreadline-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev libxtst-dev libpng-dev libxtst-dev libpng++-dev libopencv-dev libtesseract-dev zlib1g-dev libgtk2-perl
+ln -s /usr/lib/jni/libopencv_java*.so /usr/lib/libopencv_java.so
