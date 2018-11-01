744c740c5891c2be44eee9061ef50b3c0c38f211
xywang68 <xywang68@gmail.com>
2018-11-01 13:41:55 -0700

    down selenium version for IE compatibility

--- global/selenium-standalone_config.js
+++ global/selenium-standalone_config.js
@@ -1,14 +1,16 @@
+// we need to stick with 3.4.0 in order to maintain IE compatibility.
+// 3.5.0 to 3.14.0 is known to break in IE with "Error: Invalid timeout type specified: ms".
 module.exports = {
   baseURL: 'http://selenium-release.storage.googleapis.com',
-  version: '3.14.0',
+  version: '3.4.0',
   drivers: {
     chrome: {
-      version: '2.42',
+      version: '2.43',
       arch: process.arch,
       baseURL: 'http://chromedriver.storage.googleapis.com'
     },
     ie: {
-      version: '3.14.0',
+      version: '3.4.0',
       arch: process.arch,
       baseURL: 'http://selenium-release.storage.googleapis.com'
     },
@@ -23,4 +25,4 @@ module.exports = {
       baseURL: 'https://download.microsoft.com/download/D/4/1/D417998A-58EE-4EFE-A7CC-39EF9E020768/MicrosoftWebDriver.exe'
     }
   }
-}
+};
