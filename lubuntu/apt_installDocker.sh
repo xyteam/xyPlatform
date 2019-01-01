f0bbe08d2c99071203b204b30e903603ae20e572
xywang68 <xywang68@gmail.com>
2019-01-01 01:47:56 -0800

    reduce comment, no code change

--- lubuntu/apt_installDocker.sh
+++ lubuntu/apt_installDocker.sh
@@ -3,10 +3,6 @@ echo "= apt install docker-ce ="
 echo "========================="
 
 export DEBIAN_FRONTEND=noninteractive
-# apt remove -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" docker docker-engine docker.io
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -a | grep Codename | cut -d\: -f2 | xargs) stable"
-# apt update -q && apt autoremove -y
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" docker-ce
 
 cat << END1 > /etc/docker/daemon.json
--- lubuntu/apt_installJava.sh
+++ lubuntu/apt_installJava.sh
@@ -3,7 +3,5 @@ echo "= apt install oracle-java ="
 echo "==========================="
 
 export DEBIAN_FRONTEND=noninteractive
-# add-apt-repository ppa:webupd8team/java --yes --update
-# apt update -q 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --allow-unauthenticated oracle-java8-installer oracle-java8-set-default
