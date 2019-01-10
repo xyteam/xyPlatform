cd3d1f83253d05333a34bb7719a5478293bca21e
hendryang <hendryang91@gmail.com>
2019-01-10 13:21:31 +0800

    readme update and docker compose (#4)
    * install docker compose
    
    * Update readme - rsync and winrm
    
    * minor readme fix

---
+++ lubuntu/apt_installDockerCompose.sh
@@ -0,0 +1,7 @@
+echo "========================="
+echo "= install docker compose ="
+echo "========================="
+
+export DEBIAN_FRONTEND=noninteractive
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
