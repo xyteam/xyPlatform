361135e7823a7b9181ca99c6453215989d49726f
Xinyu Wang <xinyuwang@admins-mbp.na.corp.ipgnetwork.com>
2019-01-24 13:29:25 -0800

    update alias, srp do not overwrite updated file

--- lubuntu/user_setUserEnv.sh
+++ lubuntu/user_setUserEnv.sh
@@ -57,7 +57,7 @@ npm config set user $vagrant_user
 npm config set umask 0022
 
 # set alias for rsync between Projects and Run
-alias spr="rsync --human-readable --progress --archive --exclude .git/ --exclude node_modules/ $HOME/Projects/AutoBDD $HOME/Run"
+alias spr="rsync --human-readable --progress --update --archive --exclude .git/ --exclude node_modules/ $HOME/Projects/AutoBDD $HOME/Run"
 alias srp="rsync --human-readable --progress --update --exclude node_modules/ --exclude target/ $HOME/Run/ $HOME/Projects"
 
 END1
