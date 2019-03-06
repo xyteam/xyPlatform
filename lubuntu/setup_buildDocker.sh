cad0d8c67b91153016543da6552d10cee4eea8dc
Xinyu Wang <xinyuwang@admins-mbp.na.corp.ipgnetwork.com>
2019-03-06 15:21:55 -0800

    fix filemode, comment build docker during provision

--- lubuntu/setup_buildDocker.sh
+++ lubuntu/setup_buildDocker.sh
@@ -1,6 +1,6 @@
-echo "========================="
-echo "= build docker image    ="
-echo "========================="
+echo "=========================="
+echo "=   build docker image   ="
+echo "=========================="
 UBUNTU_NUM=$(lsb_release -rs | tr --delete .)
 cd ~/Projects/xyPlatform/lubuntu;
-sudo docker build --tag xyplatform1:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} .
+sudo docker build --tag xyplatform:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} .
