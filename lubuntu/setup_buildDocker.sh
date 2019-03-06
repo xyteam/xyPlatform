7fd986b204013b31430233d9f94f3e6cd6c76501
Xinyu Wang <xinyuwang@admins-mbp.na.corp.ipgnetwork.com>
2019-03-06 14:43:45 -0800

    improve version caring between 1604 and 1804

--- lubuntu/setup_buildDocker.sh
+++ lubuntu/setup_buildDocker.sh
@@ -1,6 +1,6 @@
 echo "========================="
 echo "= build docker image    ="
 echo "========================="
-
+UBUNTU_NUM=$(lsb_release -rs | tr --delete .)
 cd ~/Projects/xyPlatform/lubuntu;
-sudo docker build --tag xyplatform1:lubuntu1804 --file Dockerfile1804 .
+sudo docker build --tag xyplatform1:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} .
