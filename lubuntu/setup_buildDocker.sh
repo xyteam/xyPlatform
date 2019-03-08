72728ebf68c64334d48d5650db1ec7938fcf2277
Xinyu Wang <xinyuwang@Admins-MacBook-Pro.local>
2019-03-07 20:25:56 -0800

    support both ubuntu 18.04 and 16.04; delay docke image build after vagrant provision

--- lubuntu/setup_buildDocker.sh
+++ lubuntu/setup_buildDocker.sh
@@ -2,5 +2,5 @@ echo "=========================="
 echo "=   build docker image   ="
 echo "=========================="
 UBUNTU_NUM=$(lsb_release -rs | tr --delete .)
-cd ~/Projects/xyPlatform/lubuntu;
-sudo docker build --tag xyplatform:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} .
+cd /home/vagrant/Projects/xyPlatform/lubuntu;
+docker build --tag xyplatform:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} .
