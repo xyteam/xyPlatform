8b1cd6cfd52b5e5f4c620fd0834513258281c15e
xwvArmour <50221398+xwvArmour@users.noreply.github.com>
2019-05-11 16:56:59 -0700

    use sudor UID (#22)

--- lubuntu/setup_buildDocker.sh
+++ lubuntu/setup_buildDocker.sh
@@ -3,4 +3,4 @@ echo "=   build docker image   ="
 echo "=========================="
 UBUNTU_NUM=$(lsb_release -rs | tr --delete .)
 cd /home/vagrant/Projects/xyPlatform/lubuntu;
-docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --tag xyplatform:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} ${PWD}
+docker build --build-arg USER_ID=${SUDO_UID} --build-arg GROUP_ID=${SUDO_GID} --tag xyplatform:lubuntu${UBUNTU_NUM} --file Dockerfile${UBUNTU_NUM} ${PWD}
