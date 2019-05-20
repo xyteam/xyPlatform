1afc5f7935f39c590d53eab4e1d2d23d99c3057e
xywang68 <xywang68@gmail.com>
2019-05-20 00:21:50 -0700

    allow vnc and ssh

--- lubuntu/apt_installDocker.sh
+++ lubuntu/apt_installDocker.sh
@@ -4,7 +4,7 @@ echo "========================="
 
 export DEBIAN_FRONTEND=noninteractive
 apt install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" docker-ce
-
+sudo gpasswd -a $USER docker
 cat << END1 > /etc/docker/daemon.json
 {
   "storage-driver":"aufs",
